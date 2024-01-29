71cff3c9c5d949caa55f928e333e7b9005862143
Andrii Vasyliev <sol@hiqdev.com>
2024-01-30 00:12:36 +0200

    improve TOC to be tree

--- src/uwiki.js
+++ src/uwiki.js
@@ -1,19 +1,34 @@
 window.onload = function(event) {
   var hs = document.querySelectorAll("h2, h3, h4, h5, h6");
+  console.log(hs);
   var toc = document.getElementById("toc");
-  for (var i = 0; i < hs.length; i++) {
-    var h = hs[i];
-    if (!h.id) {
-      continue;
-    }
-    var ul = document.createElement("ul");
-    ul.id = "toc." + h.id;
-    var a = document.createElement("a");
-    a.href = "#" + h.id;
-    a.textContent = h.id;
-    var li = document.createElement("li");
-    li.appendChild(a);
-    toc.appendChild(li);
-  }
+  processHeaders(toc, hs, 0);
 };
 
+function processHeaders(parent, headers, i) {
+  var first = headers[i];
+  var li = null;
+  while (i < headers.length) {
+    var h = headers[i];
+    if (!h.id) {
+      i++;
+    } else if (h.tagName[1] == first.tagName[1]) {
+      console.log('==', h.tagName, h.id);
+      li = document.createElement("li");
+      var a = document.createElement("a");
+      a.href = "#" + h.id;
+      a.textContent = h.textContent.split(' / ').pop();
+      li.appendChild(a);
+      parent.appendChild(li);
+      i++;
+    } else if (h.tagName[1] > first.tagName[1]) {
+      console.log('> ', h.tagName, h.id);
+      var ul = document.createElement("ul");
+      li.appendChild(ul);
+      i = processHeaders(ul, headers, i);
+    } else {
+      return i;
+    }
+  }
+  return i;
+}
